fa0051027998930860abaeddc15cdb47744b0141
Nejc Graj <nejcgraj@gmail.com>
2023-01-10 17:45:10 +0100

    routing done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Outlet, Link, Route, Routes, BrowserRouter } from "react-router-dom";
 import LoginSite from './components/user-site/login-site/login-site';
 import CookieConsent from "react-cookie-consent";
 import PrivacyPolicy from './components/privacy-policy/privacy-policy';
+import FormApprovalPage from './components/admin-sites/form-approval-page/form-approval-page' 
 import UserSiteApplicationForm from './components/user-site/application-form/application-form';
+import Layout from './components/Layout/Layout.js';
 // https://www.npmjs.com/package/react-cookie-consent
 
 function App() {
   return (
     <div className="App">
-      <LoginSite/>
-
-      <CookieConsent
-        location="bottom"
-        buttonText="Potrdi"
-        declineButtonText="Zavrni"
-        cookieName="gdprCookieConsent"
-        style={{ background: "#252625" }}
-        buttonStyle={{ background: "#0abf00", color:"#ffffff"}}
-        declineButtonStyle={{ background : "#fa4a2f"}}
-        expires={1}
-        enableDeclineButton
-        onAccept={() => {
-          alert("Accepted");
-        }}
-        onDecline={() => {
-          alert("Declined");
-        }}
-      >
-        Ta stran uporablja piškotke.{" "}
-        <span style={{ fontSize: "15px" }}><a href="/privacy-policy-page">Izjava o zasebnosti</a></span>
-      </CookieConsent>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<LoginSite />} />
+          <Route path="formapprovalpage" element={<FormApprovalPage />} />
+          <Route path="usersite" element={<UserSiteApplicationForm />} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
+      <Outlet />
     </div>
   );
 }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import UserSiteApplicationForm from '../user-site/login-site/login-site';
+import PropTypes from 'prop-types';
+import ReactDOM from "react-dom/client";
+import CookieConsent from 'react-cookie-consent';
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import './Layout.css';
+
+const Layout = () => (
+  <div className="Layout">
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Login</Link>
+        </li>
+        <li>
+          <Link to="formapprovalpage">FormApprovalPage</Link>
+        </li>
+        <li>
+          <Link to="usersite">UserSite</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Outlet />
+    <CookieConsent
+        location="bottom"
+        buttonText="Potrdi"
+        declineButtonText="Zavrni"
+        cookieName="gdprCookieConsent"
+        style={{ background: "#252625" }}
+        buttonStyle={{ background: "#0abf00", color:"#ffffff"}}
+        declineButtonStyle={{ background : "#fa4a2f"}}
+        expires={1}
+        enableDeclineButton
+        onAccept={() => {
+          alert("Accepted");
+        }}
+        onDecline={() => {
+          alert("Declined");
+        }}
+      >
+        Ta stran uporablja piškotke.{" "}
+        <span style={{ fontSize: "15px" }}><a href="/privacy-policy-page">Izjava o zasebnosti</a></span>
+      </CookieConsent>
+  </div>
+);
+
+Layout.propTypes = {};
+
+Layout.defaultProps = {};
+
+export default Layout;
